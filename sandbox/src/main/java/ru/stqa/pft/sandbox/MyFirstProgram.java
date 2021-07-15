a329220171cfdb9d9b0977f00b44cc68e368ec65
Alexander <alex.kartsov@yandex.ru>
2021-07-15 11:27:39 +0300

    Добавление новых классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,14 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Alexander");
 
-		double l = 5;
-		System.out.println("Площадь квадрата с длиной стороны " + l + " равна " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата с длиной стороны " + s.l + " равна " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площать прямоугольника со сторонами" + a + " и " + b + " равна " + area(a,b));
-
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площать прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 	}
+
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
-	}
-
-	public static double area (double l) {
-		return l * l;
-	}
-
-	public static double area (double a, double b) {
-		return a * b;
 	}
 }
